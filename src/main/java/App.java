582bf7cc83cf8ebe5a849beb58ac8ebc8d58a64c
DrEremin <ivaneremin1979@gmail.com>
2021-08-03 22:22:31 +0300

    Add comparators to the class App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,10 @@
 import impl.education.*;
+import impl.enums.ComparatorsOfStudent;
+import impl.enums.ComparatorsOfUniversity;
+import impl.util.ComparatorSelector;
 import impl.util.ParseExcel;
+import interf.StudentComparator;
+import interf.UniversityComparator;
 
 import java.io.IOException;
 import java.util.LinkedList;
@@ -11,6 +16,10 @@ public class App {
                 .readStudents("src/main/resources/US.xlsx");
         LinkedList<University> listOfSUniversities = ParseExcel
                 .readUniversities("src/main/resources/US.xlsx");
+        StudentComparator studentComparator = ComparatorSelector.
+                getStudentComparator(ComparatorsOfStudent.FULL_NAME);
+        UniversityComparator universityComparator = ComparatorSelector.
+                getUniversityComparator(ComparatorsOfUniversity.FULL_NAME);
         for (Student student :listOfStudents) {
             System.out.println(student);
         }
