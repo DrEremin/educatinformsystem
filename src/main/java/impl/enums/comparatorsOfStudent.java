6943e280e9611cdb8079f9fb04f26e805ff13030
DrEremin <ivaneremin1979@gmail.com>
2021-08-03 20:47:53 +0300

    Add two enums for comparators Add enums: comparatorOfStudent and comparatorOfUniversity

---
+++ src/main/java/impl/enums/comparatorsOfStudent.java
@@ -0,0 +1,8 @@
+package impl.enums;
+
+public enum comparatorsOfStudent {
+    FULL_NAME,
+    UNIVERSITY_ID,
+    CURRENT_COURSE_NUMBER,
+    AVG_EXAM_SOURCE;
+}
---
+++ src/main/java/impl/enums/comparatorsOfUniversity.java
@@ -0,0 +1,9 @@
+package impl.enums;
+
+public enum comparatorsOfUniversity {
+    ID,
+    FULL_NAME,
+    SHORT_NAME,
+    YEAR_OF_FOUNDATION,
+    MAIN_PROFILE;
+}
