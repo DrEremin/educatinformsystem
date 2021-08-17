6d85a7d65efb88fc03a4c5f017134907ad10c9b6
DrEremin <ivaneremin1979@gmail.com>
2021-08-17 22:50:58 +0300

    Add setTitleOfColumn() to class impl.util.XlsWriter
    Add setTitleOfColumn() to class XlsWriter and
    supplemented generationFileXLXS() of class XlsWriter

--- src/main/java/AppModule35.java
+++ src/main/java/AppModule35.java
@@ -1,13 +1,11 @@
+import java.io.IOException;
+import java.util.LinkedList;
 import impl.education.Statistics;
 import impl.education.Student;
 import impl.education.University;
 import impl.util.ParseExcel;
 import impl.util.StatisticsCollector;
 import impl.util.XlsWriter;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.LinkedList;
 
 public class AppModule35 {
     public static void main(String[] args) throws IOException {
@@ -20,11 +18,11 @@ public class AppModule35 {
         LinkedList<Statistics> statisticsList = new LinkedList<>(StatisticsCollector
                 .collector(listOfUniversities, listOfStudents));
 
-        for (Statistics statistics : statisticsList) {
+        /*for (Statistics statistics : statisticsList) {
             System.out.println(statistics);
-        }
-        /*FileOutputStream fos = XlsWriter.createFileOutputStream("src/main/resources/test.txt");
-        fos.write(66);*/
+        }*/
+        XlsWriter.generationFileXLXS(statisticsList, "src/main/resources/test.xlsx");
+
 
     }
 }
--- src/main/java/impl/util/XlsWriter.java
+++ src/main/java/impl/util/XlsWriter.java
@@ -1,9 +1,9 @@
 package impl.util;
 
 import impl.education.Statistics;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -14,7 +14,11 @@ public class XlsWriter {
 
     private static XSSFWorkbook workbook;
     private static XSSFSheet sheet;
-    private static Row currentRow;
+    private static XSSFRow currentRow;
+    private static XSSFRow titleRow;
+    private static XSSFCell titleCell;
+    private static XSSFCellStyle cellStyle;
+    private static HorizontalAlignment alignment;
 
     private XlsWriter() {}
 
@@ -31,10 +35,44 @@ public class XlsWriter {
         return fos;
     }
 
+    private static void setTitleOfColumn(int index, String headerName) {
+        sheet.setColumnWidth(index, (headerName.length() + 7) * 256);
+        titleRow.createCell(index).setCellValue(headerName);
+    }
+
     public static void generationFileXLXS(Collection<Statistics> collection,
                                           String path) throws IOException {
         workbook = new XSSFWorkbook();
         sheet = workbook.createSheet("Статистика");
+        titleRow = sheet.createRow(0);
+        /*alignment = HorizontalAlignment.valueOf("CENTER");
+        cellStyle = titleRow.getRowStyle();
+        cellStyle.setAlignment(alignment);
+        titleRow.setRowStyle(cellStyle);*/
+        setTitleOfColumn(0, "Учебный профиль");
+        setTitleOfColumn(1, "Средний балл студентов");
+        setTitleOfColumn(2, "Количество студентов");
+        setTitleOfColumn(3, "Количество университетов");
+
+        int rowIndex = 1;
+        for (Statistics statistics : collection) {
+            int columnIndex = 0;
+            currentRow = sheet.createRow(rowIndex++);
+            currentRow.createCell(columnIndex++)
+                    .setCellValue(statistics
+                            .getStudyProfile()
+                            .getProfileName());
+            currentRow.createCell(columnIndex++)
+                    .setCellValue(statistics
+                            .getAvgExamScore());
+            currentRow.createCell(columnIndex++)
+                    .setCellValue(statistics
+                            .getNumberOfStudentsByProfile());
+            currentRow.createCell(columnIndex)
+                    .setCellValue(statistics
+                            .getNumberOfUniversitiesByProfile());
+        }
+
         workbook.write(createFileOutputStream(path));
     }
 }
